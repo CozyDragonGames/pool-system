5b56a6209200164e01f73558fd189ded167e8d57
KaynirGames <kaynir.games@gmail.com>
2022-04-11 21:59:39 +0300

    Добавил реализацию пула через MonoBehaviour.

---
+++ Runtime/Scripts/MonoPool.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CozyDragon.PoolSystem
+{
+    public class MonoPool<T> : MonoBehaviour, IObjectPool<T> where T : MonoBehaviour
+    {
+        [SerializeField] protected T _prefab = null;
+
+        protected Queue<T> _objects;
+
+        protected virtual void Awake() => _objects = new Queue<T>();
+
+        public void SetPrefab(T prefab) => _prefab = prefab;
+
+        public virtual T Take()
+        {
+            T obj = _objects.Count > 0
+                ? _objects.Dequeue()
+                : Instantiate(_prefab, transform);
+
+            obj.gameObject.SetActive(true);
+
+            return obj;
+        }
+
+        public virtual void Return(T obj)
+        {
+            obj.gameObject.SetActive(false);
+
+            _objects.Enqueue(obj);
+        }
+
+        public virtual void Clear()
+        {
+            for (int i = 0; i < _objects.Count; i++)
+            {
+                Destroy(_objects.Dequeue().gameObject);
+            }
+        }
+    }
+}
